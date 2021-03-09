27157455834a18a41b696ea3bba2dc0b5b059ef4
declarative-caller-2545 <nolanghiggins@gmail.com>
2021-03-09 00:13:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
-function basicTeenager(age) {
-  if(age <= 19 && age >= 13){
+function basicTeenager(age){
+  if(age <= 19 && age >= 13) {
     return 'You are a teenager!'
   }
 }
@@ -7,12 +7,12 @@ function basicTeenager(age) {
 function teenager(age) {
       if (age <= 19 && age >= 13) {
         return 'You are a teenager!'
-    } else {
+    } 
+    else {
         return 'You are not a teenager'
     }
 }
 
-}
 
 function ageChecker(age) {
 
